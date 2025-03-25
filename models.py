c14557bf01eba6d4cc9fb1bb213e5230189803a5
Duy Hoàng <duypo03cbvn@gmail.com>
2025-03-25 15:34:55 +0700

    Thêm dropout vào MLP

--- models.py
+++ models.py
@@ -69,16 +69,22 @@ class fMLP(nn.Module):
         self.layer1 = nn.Linear(28*28, 200)
         self.layer2 = nn.Linear(200, 200)
         self.layer3 = nn.Linear(200, 10)
-        self.dropout = nn.Dropout(p=0.5)
+
+        self.dropout1 = nn.Dropout(p=0.3)
+        self.dropout2 = nn.Dropout(p=0.5)
         self.relu = nn.ReLU()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.flatten(x)
+
         x = self.layer1(x)
         x = self.relu(x)
+        x = self.dropout1(x) 
+
         x = self.layer2(x)
         x = self.relu(x)
-        x = self.dropout(x)
+        x = self.dropout2(x)
+        
         x = self.layer3(x)
         return x
 
